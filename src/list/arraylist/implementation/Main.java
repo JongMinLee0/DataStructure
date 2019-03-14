2f2c588492d166db52a498475070bda8ceac7ef2
JongMinLee <1393062@naver.com>
2019-03-14 21:00:56 +0900

    add : - Data Structure / Array List 중간 실습

---
+++ src/list/arraylist/implementation/Main.java
@@ -0,0 +1,36 @@
+package list.arraylist.implementation;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList numbers = new ArrayList();
+		numbers.addLast(10);
+		numbers.addLast(20);
+		numbers.addLast(30);
+		numbers.addLast(40);
+		numbers.add(1, 15);
+		numbers.addFirst(5);
+		numbers.removeFirst();
+		numbers.removeLast();
+//		System.out.println(numbers.get(0));
+//		System.out.println(numbers.get(1));
+//		System.out.println(numbers.get(2));
+//		System.out.println(numbers.remove(1));
+//		System.out.println(numbers);
+//		System.out.println(numbers.size());
+//		System.out.println(numbers.indexOf(20));
+		
+//		for (int i=0; i<numbers.size(); i++) {
+//			System.out.println(numbers.get(i));
+//		}
+		ArrayList.ListIterator li = numbers.listIterator();
+//		while(li.hasNext()) {
+//			System.out.println(li.next());
+//		}
+		li.previous();
+		li.previous();
+		
+	}
+
+}
